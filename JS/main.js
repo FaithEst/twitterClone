5b72ccab4d62b732f6e936454180d877442258d3
Faith Mutitu <mutitufaith53@gmail.com>
2022-02-09 13:40:17 +0300

    date converted to string

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,20 @@
 document.getElementById("tweetBtn1").onclick = function(){
-   let whatsHappening = document.getElementById("tweetBtn1").value;
+   let whatsHappening = document.getElementById("whatsHappening").value;
    let tweetDate = new Date();
    firebase.firestore().collection("tweet").doc().set({
         tweet: whatsHappening,
         date: tweetDate
    });
+   firebase.firestore().collection("tweet").get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+
+         let timeStamp = doc.data().date;
+
+         let dateConvert = timeStamp.toDate().toDateString();
+         let dateTime = document.getElementById("dateTime");
+         dateTime.innerHTML = dateConvert;
+         
+         console.log("success");
+      });
+   })
 }
